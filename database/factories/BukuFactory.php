83a77be63e81d9b9baf4cd736a22be41cb3d1f8c
farhandito15 <farhandito15@gmail.com>
2022-09-21 15:00:23 +0700

    apakah factory ini yang dimaksud mas ??

---
+++ database/factories/BukuFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class BukuFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'list' => $this->faker->sentence(),
+            'mark' => rand(1, 60),
+        ];
+    }
+}
